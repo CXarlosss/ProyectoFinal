394b54c184f6211a3833958a6b2792ac064e2627
Carlos <carlosdepet@gmail.com>
2025-02-11 13:19:50 +0100

    Mirar ErrC

--- Proyecto/server/server-express.js
+++ Proyecto/server/server-express.js
@@ -5,7 +5,7 @@ import { db } from "./server-mongodb.js";
 
 
 const app = express();
-const port =  process.env.PORT;
+const port =  process.env.PORT || 3001;
 
 
 
@@ -26,31 +26,31 @@ app.get('/check/:nombre', async (req, res) => {
  // CRUD
 app.post('/create/servicios', async (req, res) => {
   console.log("📌 Servicio recibido:", req.body);
-  res.json(await db.Servicios.create(req.body))
+  res.json(await db.servicios.create(req.body))
 }) 
 app.get('/read/servicios',async (req, res) => {
   console.log("📌 Servicio Creado:", req.body);
-  res.json(await db.Servicios.read(req.params.id, req.body))
+  res.json(await db.servicios.get())
 });
 app.put('/update/servicios/:_id', async(req, res) => {
-  res.json(await db.Servicios.update(req.params.id, req.body))
+  res.json(await db.servicios.update(req.params.id, req.body))
   console.log(`📌 Recibiendo actualización para servicio _id: ${req.params._id}`, req.body);
   });
 app.delete('/delete/servicios/:_id', async (req, res) => {
   console.log(`📌 Eliminando servicio con _id: ${req.params._id}`);
-  res.json(await db.Servicios.delete(req.params.id))
+  res.json(await db.servicios.delete(req.params.id))
 });
 
 app.post('/create/users', async (req, res) => {
   console.log("📌 Usuario recibido:", req.body);
-  res.json(await db.Users.create(req.body))
+  res.json(await db.users.create(req.body))
 }) 
 app.get('/read/users',async (req, res) => {
   console.log("📌 Users Creado:", req.body);
-  res.json(await db.Users.read(req.params.id, req.body))
+  res.json(await db.users.get())
 });
 app.put('/update/users/:_id', async(req, res) => {
-  res.json(await db.Users.update(req.params.id, req.body))
+  res.json(await db.users.update(req.params.id, req.body))
   console.log(`📌 Recibiendo actualización para Usuario_id: ${req.params._id}`, req.body);
   });
   app.delete('/delete/users/:_id', async (req, res) => {

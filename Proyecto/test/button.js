d445243daaeee370aa0885614a7a805d5050e240
Carlos <carlosdepet@gmail.com>
2025-03-03 19:33:26 +0100

    1er TEst

---
+++ Proyecto/test/button.js
@@ -0,0 +1,5 @@
+// Función que simula un botón con evento de clic
+export default function suma(a, b) {
+    return a + b;
+  }
+  
---
+++ Proyecto/test/button.test.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-undef */
+// @ts-nocheck
+
+
+// @ts-ignoreimport suma from "../button.js";
+
+import suma from "./button.js";
+
+test("suma 2 + 3 para que sea 5", () => {
+ 
+    expect(suma(2, 3)).toBe(5);
+});
